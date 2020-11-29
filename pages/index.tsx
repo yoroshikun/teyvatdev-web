11453f2f284526c6dca1db7841f096a2c03158c6
Ryan Yang <ryanqyang@gmail.com>
2020-11-29 11:15:50 -0800

    Fulfilled basic requirements. Added static icon assets as well as created custom types file for png images. Responsive for both mobile + desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,34 @@ const Home = (): React.ReactNode => {
     <div className={styles.container}>
       <Head>
         <title>Teyvat Dev</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
+      <div className={styles.hero}>
+        coming soon
+      </div>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Teyvat <span>Dev</span>
         </h1>
 
-        <p className={styles.description}>Coming soon</p>
-        {/* Edit here, have fun! */}
-        {/* You should use a slime somewhere (images in the public) */}
+        <div className={styles.socials}>
+          <p>Find us on:</p>
+          <div className={styles.icons}>
+            <a href="https://discord.gg/t9kT4J3w" target="_blank" rel="noreferrer noopener">
+              <img src="/icons/discord.png" />
+            </a>
+            <a href="https://github.com/yoroshikun/teyvatdev-web" target="_blank" rel="noreferrer noopener">
+              <img src="/icons/github.png" />
+            </a>
+            <a href="mailto:contact@teyvat.dev" target="_blank" rel="noreferrer noopener">
+              <img src="/icons/email.png" />
+            </a>
+          </div>
+        </div>
       </main>
 
-      <footer className={styles.footer}>{/* Edit here */}</footer>
+      <footer className={styles.footer}>Teyvat.dev is not affiliated with or endorsed by miHoYo. </footer>
     </div>
   );
 };
